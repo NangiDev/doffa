cf314ea8ce8639d819719a12d6f211fe2b56d182
Joel Setterberg <j.settrbrg@gmail.com>
2020-04-24 08:21:09 +0200

    Gui mockup

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 from tkinter import *
 from tkinter import simpledialog
+from tkcalendar import Calendar
+
+import json
+import calculate as calc
+
 
 class Window(Frame):
 
@@ -8,6 +13,19 @@ class Window(Frame):
         Frame.__init__(self, master)
         self.master = master
         self.init_window()
+
+    def init_cal(self, date, row=0, col=0):
+        cal = Calendar(self, year=2018, month=2, day=5)
+        cal.grid(row=row,column=col)
+
+    def init_button(self, row=0, col=0):
+        button = Button(self, text="C\nA\nL\nC")
+        button.grid(row=row,column=col)
+
+    def init_text(self, row=0, col=0):
+        text = Text(self, state='disabled')
+        text.grid(row=row,column=col)
+
 
     #Creation of init_window
     def init_window(self):
@@ -20,21 +38,27 @@ class Window(Frame):
         
         menu.add_command(label="Token", command=self.typeToken)
 
+        self.init_cal("", 0,0)
+        self.init_button( 0,1)
+        self.init_text(0,2)
+
+        self.init_cal("", 1,0)
+        self.init_button( 1,1)
+        self.init_text(1,2)
+
+        #end_text.insert(INSERT, json.dumps(calc.parseJson(calc.request("2020-04-08")), indent=2))
+
     def typeToken(self):
         title="API token"
         prompt="Enter your API token"
 
-        file_name="token.txt"
-        with open(file_name, "r") as f:
+        with open(calc.file_name, "r") as f:
             lines = f.read().splitlines()
-            if len(lines) > 0:
-                initialValue=lines[0]
-            else:
-                initialValue=""
+            initialValue = lines[0] if len(lines) > 0 else ""
 
         user_input = simpledialog.askstring(title=title, prompt=prompt, initialvalue=initialValue) or initialValue
         if user_input != initialValue:
-            with open(file_name, "w+") as f:
+            with open(calc.file_name, "w+") as f:
                 f.write(user_input)
 
 def main():
@@ -42,9 +66,8 @@ def main():
         root.destroy()
 
     root = Tk()
-    root.geometry("400x300")
 
-    root.eval('tk::PlaceWindow %s center' % root.winfo_toplevel())
+    #root.eval('tk::PlaceWindow %s center' % root.winfo_toplevel())
 
     Window(root)
 
