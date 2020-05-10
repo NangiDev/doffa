9b0a1afd74698427873cab41a2d5b703248e96b9
Joel <j.settrbrg@gmail.com>
2020-05-10 12:56:54 +0200

    Trying to get vue to load

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
         ? '/doffa/'
+        : '/',
+    publicPath: process.env.NODE_ENV === 'development'
+        ? '/doffa/'
         : '/'
 }
