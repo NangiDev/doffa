7c79b2632594f6300624ebc2db1b030f871be0d5
Joel Setterberg <j.settrbrg@gmail.com>
2020-11-03 10:37:07 +0100

    Remove old python fils

--- build.ps1
+++
@@ -1,8 +0,0 @@
-Set-Alias exist Test-Path -Option "Constant, AllScope"
-
-if (exist __pycache) { Remove-Item -Path __pycache__ -Recurse }
-if (exist build) { Remove-Item -Path build -Recurse }
-if (exist dist) { Remove-Item -Path dist -Recurse }
-if (exist main.spec) { Remove-Item -Path main.spec }
-
-pyinstaller --onefile --hidden-import babel.numbers main.py
--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rm -rf __pycache__/ main.spec dist/ build/
-pyinstaller --onefile --hidden-import babel.numbers main.py
