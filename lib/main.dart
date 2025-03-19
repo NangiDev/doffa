a15250f0fc4636255bdc12d0d7037d8de593b800
Joel Setterberg <joel.setterberg@volvocars.com>
2025-03-19 18:46:27 +0100

    Some structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,116 +8,227 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Doffa - Fitness Tracker',
+      theme: ThemeData.from(colorScheme: ColorScheme.light()),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+  const MyHomePage({super.key});
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return Center(
+      child: Card(
+        child: Padding(padding: EdgeInsets.all(32.0), child: MyDoffa()),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+    );
+  }
+}
+
+class MyDoffa extends StatelessWidget {
+  const MyDoffa({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Flex(
+      spacing: 16.0,
+      direction: Axis.vertical,
+      children: [
+        MyLogo(),
+        MyDatePickers(),
+        MyGraph(),
+        MyData(),
+        MyProgress(),
+        MyRatio(),
+        GoogleAds(),
+      ],
+    );
+  }
+}
+
+class GoogleAds extends StatelessWidget {
+  const GoogleAds({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class MyRatio extends StatelessWidget {
+  const MyRatio({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Flex(
+      direction: Axis.horizontal,
+      children: [
+        Expanded(
+          child: TextField(
+            decoration: InputDecoration(
+              labelText: 'Progress',
+              hintText: 'Enter Progress',
+              prefixIcon: Icon(Icons.trending_up),
             ),
-          ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        Expanded(
+          child: TextField(
+            decoration: InputDecoration(
+              labelText: 'Goal',
+              hintText: 'Enter Goal',
+              prefixIcon: Icon(Icons.flag),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class MyProgress extends StatelessWidget {
+  const MyProgress({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Flex(
+      direction: Axis.vertical,
+      children: [
+        Expanded(
+          child: TextField(
+            decoration: InputDecoration(
+              labelText: 'Progress',
+              hintText: 'Enter Progress',
+              prefixIcon: Icon(Icons.trending_up),
+            ),
+          ),
+        ),
+        Expanded(
+          child: TextField(
+            decoration: InputDecoration(
+              labelText: 'Goal',
+              hintText: 'Enter Goal',
+              prefixIcon: Icon(Icons.flag),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class MyData extends StatelessWidget {
+  const MyData({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Flex(
+      direction: Axis.horizontal,
+      children: [
+        Expanded(
+          child: TextField(
+            decoration: InputDecoration(
+              labelText: 'Weight',
+              hintText: 'Enter Weight',
+              prefixIcon: Icon(Icons.line_weight),
+            ),
+          ),
+        ),
+        Expanded(
+          child: TextField(
+            decoration: InputDecoration(
+              labelText: 'Height',
+              hintText: 'Enter Height',
+              prefixIcon: Icon(Icons.height),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class MyGraph extends StatelessWidget {
+  const MyGraph({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class MyDatePickers extends StatelessWidget {
+  const MyDatePickers({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Flex(
+      direction: Axis.horizontal,
+      spacing: 16.0,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Card(
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.calendar_today),
+                  onPressed: () {
+                    showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime(2000),
+                      lastDate: DateTime(2100),
+                    );
+                  },
+                ),
+                Text(
+                  'Start Date',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.normal),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Card(
+          child: IconButton(
+            icon: Icon(Icons.calendar_today),
+            onPressed: () {
+              showDatePicker(
+                context: context,
+                initialDate: DateTime.now(),
+                firstDate: DateTime(2000),
+                lastDate: DateTime(2100),
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class MyLogo extends StatelessWidget {
+  const MyLogo({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Flex(
+      direction: Axis.vertical,
+      children: [
+        SvgPicture.asset(
+          './assets/opt_prism_dark.svg',
+          semanticsLabel: 'App Logo',
+        ),
+        Text(
+          'DOFFA',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+      ],
     );
   }
 }
