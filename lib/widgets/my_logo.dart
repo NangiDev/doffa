1b3b4922d7616e1eaf59724cd1a1f1d379cb481b
Joel Setterberg <joel.setterberg@volvocars.com>
2025-03-20 17:12:16 +0100

    Added more stuff

--- lib/widgets/my_logo.dart
+++ lib/widgets/my_logo.dart
@@ -5,24 +5,18 @@ class MyLogo extends StatelessWidget {
   const MyLogo({super.key});
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            SvgPicture.asset(
-              './assets/opt_prism_dark.svg',
-              semanticsLabel: 'App Logo',
-            ),
-            Text(
-              'DOFFA',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
-            ),
-          ],
+    return Column(
+      children: [
+        SvgPicture.asset(
+          './assets/opt_prism_dark.svg',
+          semanticsLabel: 'App Logo',
         ),
-      ),
+        Text(
+          'DOFFA',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          textAlign: TextAlign.center,
+        ),
+      ],
     );
   }
 }
