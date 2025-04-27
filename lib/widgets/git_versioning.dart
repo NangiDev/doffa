5b5eb4d0a1d05a3f6d0c434448c3f536b0efa904
Joel Setterberg <j.settrbrg@gmail.com>
2025-04-27 11:52:42 +0200

    Update git version widget

--- lib/widgets/git_versioning.dart
+++ lib/widgets/git_versioning.dart
@@ -1,18 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class GitVersioningWidget extends StatelessWidget {
   const GitVersioningWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Text(
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(
           'Version: ${const String.fromEnvironment('GIT_HASH', defaultValue: 'Unknown')}',
-          style: const TextStyle(fontSize: 16, color: Colors.white),
+          style: GoogleFonts.montserrat(
+            fontSize: 12,
+            color: Colors.white,
+            fontWeight: FontWeight.w100,
+            height: 1,
+          ),
+          textAlign: TextAlign.center,
         ),
-      ),
+        const SizedBox(height: 4),
+        Text(
+          '© 2020 Doffa. All rights reserved.',
+          style: GoogleFonts.montserrat(
+            fontSize: 12,
+            color: Colors.white,
+            fontWeight: FontWeight.w100,
+            height: 1,
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ],
     );
   }
 }
