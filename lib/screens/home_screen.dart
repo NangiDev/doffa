f2258bccf09af0038413288d00be3178e285293a
Joel Setterberg <j.settrbrg@gmail.com>
2025-04-27 23:13:35 +0200

    Added scaffold for missing cardsg

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:doffa/widgets/my_ads.dart';
 import 'package:doffa/widgets/my_date_picker_card.dart';
 import 'package:doffa/widgets/my_logo_wide.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -12,9 +13,7 @@ class HomeScreen extends StatelessWidget {
     return SingleChildScrollView(
       child: Center(
         child: ConstrainedBox(
-          constraints: BoxConstraints(
-            maxWidth: 400, // Set max width (adjust as needed)
-          ),
+          constraints: BoxConstraints(maxWidth: 400),
           child: Padding(
             padding: const EdgeInsets.all(16.0),
             child: Column(
@@ -23,6 +22,10 @@ class HomeScreen extends StatelessWidget {
                 MyLogoWide(),
                 MyDatePickerCard(),
                 MyAds(),
+                MyRatioCard(),
+                MyHistoryCard(),
+                MyDataCard(),
+                MyProgressCard(),
                 MyAds(),
                 MyAds(),
                 GitVersioningWidget(),
@@ -31,6 +34,194 @@ class HomeScreen extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+}
+
+class MyRatioCard extends StatelessWidget {
+  const MyRatioCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        double maxWidth = constraints.maxWidth;
+        return Container(
+          width: maxWidth,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color(0xFF3A3A3A), Color(0xFF222222)],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+            borderRadius: BorderRadius.all(Radius.circular(8)),
+            border: Border.all(color: Color(0xFF606060), width: 2),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              spacing: 16,
+              children: [
+                Text(
+                  "Ratio",
+                  style: GoogleFonts.montserrat(
+                    fontSize: maxWidth / 24,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                    height: 1,
+                  ),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class MyProgressCard extends StatelessWidget {
+  const MyProgressCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        double maxWidth = constraints.maxWidth;
+        return Container(
+          width: maxWidth,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color(0xFF3A3A3A), Color(0xFF222222)],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+            borderRadius: BorderRadius.all(Radius.circular(8)),
+            border: Border.all(color: Color(0xFF606060), width: 2),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              spacing: 16,
+              children: [
+                Text(
+                  "Progress",
+                  style: GoogleFonts.montserrat(
+                    fontSize: maxWidth / 24,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                    height: 1,
+                  ),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class MyHistoryCard extends StatelessWidget {
+  const MyHistoryCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        double maxWidth = constraints.maxWidth;
+        return Container(
+          width: maxWidth,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color(0xFF3A3A3A), Color(0xFF222222)],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+            borderRadius: BorderRadius.all(Radius.circular(8)),
+            border: Border.all(color: Color(0xFF606060), width: 2),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              spacing: 16,
+              children: [
+                Text(
+                  "History",
+                  style: GoogleFonts.montserrat(
+                    fontSize: maxWidth / 24,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                    height: 1,
+                  ),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class MyDataCard extends StatelessWidget {
+  const MyDataCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        double maxWidth = constraints.maxWidth;
+        return Container(
+          width: maxWidth,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color(0xFF3A3A3A), Color(0xFF222222)],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+            borderRadius: BorderRadius.all(Radius.circular(8)),
+            border: Border.all(color: Color(0xFF606060), width: 2),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              spacing: 16,
+              children: [
+                Text(
+                  "Data",
+                  style: GoogleFonts.montserrat(
+                    fontSize: maxWidth / 24,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                    height: 1,
+                  ),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
