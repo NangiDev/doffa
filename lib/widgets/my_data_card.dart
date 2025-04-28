fe70babe972b75443f635f94fb64546d9aed9fe0
Joel Setterberg <j.settrbrg@gmail.com>
2025-04-28 17:24:11 +0200

    nice looking data table

--- lib/widgets/my_data_card.dart
+++ lib/widgets/my_data_card.dart
@@ -99,69 +99,423 @@ class _MyDataCardState extends State<MyDataCard> {
   }
 
   Widget _buildExpandedContent(double maxWidth) {
-    return Container(
-      width: maxWidth,
-      decoration: const BoxDecoration(
-        color: Color.fromARGB(255, 55, 55, 55),
-        borderRadius: BorderRadius.vertical(bottom: Radius.circular(8)),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // Metric Column
-            Expanded(
-              child: _buildColumn(
-                maxWidth: maxWidth,
-                fontWeight: FontWeight.w600,
-                alignment: CrossAxisAlignment.start,
-                values: const [
-                  "Metric",
-                  "BMI",
-                  "Fat (%)",
-                  "Fat (kg)",
-                  "Lean (kg)",
-                  "Weight (kg)",
+    return Column(
+      children: [
+        Container(
+          width: maxWidth,
+          decoration: BoxDecoration(
+            color: Color.fromARGB(255, 55, 55, 55),
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(8),
+              bottomRight: Radius.circular(8),
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: Row(
+                spacing: 16,
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Text(
+                      "Metric",
+                      style: GoogleFonts.montserrat(
+                        fontSize: maxWidth / 24,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w500,
+                        height: 1,
+                      ),
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        "2025-01-01",
+                        style: GoogleFonts.montserrat(
+                          fontSize: maxWidth / 24,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500,
+                          height: 1,
+                        ),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        "2025-05-25",
+                        style: GoogleFonts.montserrat(
+                          fontSize: maxWidth / 24,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500,
+                          height: 1,
+                        ),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ),
-            // First Date Column
-            Expanded(
-              child: _buildColumn(
-                maxWidth: maxWidth,
-                fontWeight: FontWeight.w300,
-                alignment: CrossAxisAlignment.center,
-                values: const [
-                  "2025-01-01",
-                  "24.5",
-                  "18.2",
-                  "14.6",
-                  "65.3",
-                  "80.1",
-                ],
-              ),
-            ),
-            // Second Date Column
-            Flexible(
-              flex: 0,
-              child: _buildColumn(
-                maxWidth: maxWidth,
-                fontWeight: FontWeight.w300,
-                alignment: CrossAxisAlignment.center,
-                values: const [
-                  "2025-05-25",
-                  "24.5",
-                  "18.2",
-                  "14.6",
-                  "65.3",
-                  "80.1",
-                ],
-              ),
-            ),
-          ],
+          ),
         ),
-      ),
+        Container(
+          width: maxWidth,
+          decoration: BoxDecoration(
+            color: Color.fromARGB(255, 35, 35, 35),
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(8),
+              bottomRight: Radius.circular(8),
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.only(
+              bottom: 8.0,
+              left: 16.0,
+              right: 16.0,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 4.0),
+                  child: Column(
+                    children: [
+                      Row(
+                        spacing: 16,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Expanded(
+                            flex: 1,
+                            child: Text(
+                              "BMI",
+                              style: GoogleFonts.montserrat(
+                                fontSize: maxWidth / 32,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w300,
+                                height: 1,
+                              ),
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: Align(
+                              alignment: Alignment.center,
+                              child: Text(
+                                "29.5",
+                                style: GoogleFonts.montserrat(
+                                  fontSize: maxWidth / 32,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w300,
+                                  height: 1,
+                                ),
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: Align(
+                              alignment: Alignment.center,
+                              child: Text(
+                                "29.5",
+                                style: GoogleFonts.montserrat(
+                                  fontSize: maxWidth / 32,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w300,
+                                  height: 1,
+                                ),
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const Divider(
+                        color: Colors.grey,
+                        thickness: 1,
+                        height: 2,
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 4.0),
+                  child: Column(
+                    children: [
+                      Row(
+                        spacing: 16,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Expanded(
+                            flex: 1,
+                            child: Text(
+                              "Fat (%)",
+                              style: GoogleFonts.montserrat(
+                                fontSize: maxWidth / 32,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w300,
+                                height: 1,
+                              ),
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: Align(
+                              alignment: Alignment.center,
+                              child: Text(
+                                "29.5",
+                                style: GoogleFonts.montserrat(
+                                  fontSize: maxWidth / 32,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w300,
+                                  height: 1,
+                                ),
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: Align(
+                              alignment: Alignment.center,
+                              child: Text(
+                                "29.5",
+                                style: GoogleFonts.montserrat(
+                                  fontSize: maxWidth / 32,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w300,
+                                  height: 1,
+                                ),
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const Divider(
+                        color: Colors.grey,
+                        thickness: 1,
+                        height: 2,
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 4.0),
+                  child: Column(
+                    children: [
+                      Row(
+                        spacing: 16,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Expanded(
+                            flex: 1,
+                            child: Text(
+                              "Fat (kg)",
+                              style: GoogleFonts.montserrat(
+                                fontSize: maxWidth / 32,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w300,
+                                height: 1,
+                              ),
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: Align(
+                              alignment: Alignment.center,
+                              child: Text(
+                                "29.5",
+                                style: GoogleFonts.montserrat(
+                                  fontSize: maxWidth / 32,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w300,
+                                  height: 1,
+                                ),
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: Align(
+                              alignment: Alignment.center,
+                              child: Text(
+                                "29.5",
+                                style: GoogleFonts.montserrat(
+                                  fontSize: maxWidth / 32,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w300,
+                                  height: 1,
+                                ),
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const Divider(
+                        color: Colors.grey,
+                        thickness: 1,
+                        height: 2,
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 4.0),
+                  child: Column(
+                    children: [
+                      Row(
+                        spacing: 16,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Expanded(
+                            flex: 1,
+                            child: Text(
+                              "Lean (kg)",
+                              style: GoogleFonts.montserrat(
+                                fontSize: maxWidth / 32,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w300,
+                                height: 1,
+                              ),
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: Align(
+                              alignment: Alignment.center,
+                              child: Text(
+                                "29.5",
+                                style: GoogleFonts.montserrat(
+                                  fontSize: maxWidth / 32,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w300,
+                                  height: 1,
+                                ),
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: Align(
+                              alignment: Alignment.center,
+                              child: Text(
+                                "29.5",
+                                style: GoogleFonts.montserrat(
+                                  fontSize: maxWidth / 32,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w300,
+                                  height: 1,
+                                ),
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const Divider(
+                        color: Colors.grey,
+                        thickness: 1,
+                        height: 2,
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 4.0),
+                  child: Row(
+                    spacing: 16,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: Text(
+                          "Weight (kg)",
+                          style: GoogleFonts.montserrat(
+                            fontSize: maxWidth / 32,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w300,
+                            height: 1,
+                          ),
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: Text(
+                            "29.5",
+                            style: GoogleFonts.montserrat(
+                              fontSize: maxWidth / 32,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w300,
+                              height: 1,
+                            ),
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: Text(
+                            "29.5",
+                            style: GoogleFonts.montserrat(
+                              fontSize: maxWidth / 32,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w300,
+                              height: 1,
+                            ),
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 
