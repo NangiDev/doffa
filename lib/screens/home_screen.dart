0fa183f94ffd4ba5abe77fdd1a9861f3fdff8f73
Joel Setterberg <j.settrbrg@gmail.com>
2025-04-28 10:30:44 +0200

    Expanding card logic for history, data and progress

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,12 +1,14 @@
 import 'package:doffa/widgets/git_versioning.dart';
 import 'package:doffa/widgets/my_ads.dart';
 import 'package:doffa/widgets/my_coffee_button.dart';
+import 'package:doffa/widgets/my_data_card.dart';
 import 'package:doffa/widgets/my_date_picker_card.dart';
+import 'package:doffa/widgets/my_history_card.dart';
 import 'package:doffa/widgets/my_logo_wide.dart';
+import 'package:doffa/widgets/my_progress_card.dart';
 import 'package:doffa/widgets/my_ratio_card.dart';
 import 'package:doffa/widgets/my_sign_out_button.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -30,7 +32,7 @@ class HomeScreen extends StatelessWidget {
                 MyDatePickerCard(),
                 MyAds(),
                 MyRatioCard(),
-                // MyHistoryCard(),
+                MyHistoryCard(),
                 MyDataCard(),
                 MyProgressCard(),
                 MyAds(),
@@ -41,227 +43,6 @@ class HomeScreen extends StatelessWidget {
           ),
         ),
       ),
-    );
-  }
-}
-
-class MyProgressCard extends StatelessWidget {
-  const MyProgressCard({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        double maxWidth = constraints.maxWidth;
-        return Container(
-          width: maxWidth,
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [Color(0xFF3A3A3A), Color(0xFF222222)],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-            borderRadius: BorderRadius.all(Radius.circular(8)),
-            border: Border.all(color: Color(0xFF606060), width: 2),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.only(top: 16, left: 16),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              spacing: 8,
-              children: [
-                Text(
-                  "PROGRESS",
-                  style: GoogleFonts.montserrat(
-                    fontSize: maxWidth / 18,
-                    color: Colors.white,
-                    fontWeight: FontWeight.w500,
-                    height: 1,
-                  ),
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                Text(
-                  "Changes between start and end dZate",
-                  style: GoogleFonts.montserrat(
-                    fontSize: maxWidth / 32,
-                    color: Colors.white,
-                    fontWeight: FontWeight.w100,
-                    height: 1,
-                  ),
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                Container(
-                  width: maxWidth,
-                  decoration: BoxDecoration(
-                    color: Color.fromARGB(255, 55, 55, 55),
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(8),
-                      bottomRight: Radius.circular(8),
-                    ),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      spacing: 8,
-                      children: [
-                        Text(
-                          "100",
-                          style: GoogleFonts.montserrat(
-                            fontSize: maxWidth / 6,
-                            color: Color.fromARGB(255, 109, 255, 129),
-                            fontWeight: FontWeight.w900,
-                            height: 1,
-                          ),
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        );
-      },
-    );
-  }
-}
-
-class MyHistoryCard extends StatelessWidget {
-  const MyHistoryCard({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        double maxWidth = constraints.maxWidth;
-        return Container(
-          width: maxWidth,
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [Color(0xFF3A3A3A), Color(0xFF222222)],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-            borderRadius: BorderRadius.all(Radius.circular(8)),
-            border: Border.all(color: Color(0xFF606060), width: 2),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              spacing: 16,
-              children: [
-                Text(
-                  "History",
-                  style: GoogleFonts.montserrat(
-                    fontSize: maxWidth / 24,
-                    color: Colors.white,
-                    fontWeight: FontWeight.w600,
-                    height: 1,
-                  ),
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                ),
-              ],
-            ),
-          ),
-        );
-      },
-    );
-  }
-}
-
-class MyDataCard extends StatelessWidget {
-  const MyDataCard({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        double maxWidth = constraints.maxWidth;
-        return Container(
-          width: maxWidth,
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [Color(0xFF3A3A3A), Color(0xFF222222)],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-            borderRadius: BorderRadius.all(Radius.circular(8)),
-            border: Border.all(color: Color(0xFF606060), width: 2),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.only(top: 16, left: 16),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              spacing: 8,
-              children: [
-                Text(
-                  "DATA",
-                  style: GoogleFonts.montserrat(
-                    fontSize: maxWidth / 18,
-                    color: Colors.white,
-                    fontWeight: FontWeight.w500,
-                    height: 1,
-                  ),
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                Text(
-                  "Measurements",
-                  style: GoogleFonts.montserrat(
-                    fontSize: maxWidth / 32,
-                    color: Colors.white,
-                    fontWeight: FontWeight.w100,
-                    height: 1,
-                  ),
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                Container(
-                  width: maxWidth,
-                  decoration: BoxDecoration(
-                    color: Color.fromARGB(255, 55, 55, 55),
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(8),
-                      bottomRight: Radius.circular(8),
-                    ),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      spacing: 8,
-                      children: [
-                        Text(
-                          "100",
-                          style: GoogleFonts.montserrat(
-                            fontSize: maxWidth / 6,
-                            color: Color.fromARGB(255, 109, 255, 129),
-                            fontWeight: FontWeight.w900,
-                            height: 1,
-                          ),
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        );
-      },
     );
   }
 }
---
+++ lib/widgets/my_progress_card.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MyProgressCard extends StatefulWidget {
+  const MyProgressCard({super.key});
+
+  @override
+  MyProgressCardState createState() => MyProgressCardState();
+}
+
+class MyProgressCardState extends State<MyProgressCard> {
+  bool _isExpanded = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        double maxWidth = constraints.maxWidth;
+
+        return Container(
+          width: maxWidth,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color(0xFF3A3A3A), Color(0xFF222222)],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+            borderRadius: BorderRadius.all(Radius.circular(8)),
+            border: Border.all(color: Color(0xFF606060), width: 2),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      spacing: 8,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "PROGRESS",
+                          style: GoogleFonts.montserrat(
+                            fontSize: maxWidth / 18,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500,
+                            height: 1,
+                          ),
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        Text(
+                          "Changes between start and end date",
+                          style: GoogleFonts.montserrat(
+                            fontSize: maxWidth / 32,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w100,
+                            height: 1,
+                          ),
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ],
+                    ),
+                    IconButton(
+                      icon: Icon(
+                        _isExpanded
+                            ? Icons.keyboard_arrow_up
+                            : Icons.keyboard_arrow_down,
+                        color: Colors.white,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _isExpanded = !_isExpanded;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              AnimatedCrossFade(
+                duration: const Duration(milliseconds: 300),
+                firstChild: Container(),
+                secondChild: Container(
+                  width: maxWidth,
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 55, 55, 55),
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(8),
+                      bottomRight: Radius.circular(8),
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      children: [
+                        Text(
+                          "100",
+                          style: GoogleFonts.montserrat(
+                            fontSize: maxWidth / 6,
+                            color: Color.fromARGB(255, 109, 255, 129),
+                            fontWeight: FontWeight.w900,
+                            height: 1,
+                          ),
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        Text(
+                          "Additional content displayed when expanded",
+                          style: GoogleFonts.montserrat(
+                            fontSize: maxWidth / 32,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w200,
+                            height: 1,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                crossFadeState:
+                    _isExpanded
+                        ? CrossFadeState.showSecond
+                        : CrossFadeState.showFirst,
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
