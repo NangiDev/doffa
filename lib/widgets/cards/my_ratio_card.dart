b8848ab12f54fab788ad971d967d4b1ae88d40ec
Joel Setterberg <j.settrbrg@gmail.com>
2025-05-05 10:43:44 +0200

    Added description for how ratio is calculated. Also added the grade table

--- lib/widgets/cards/my_ratio_card.dart
+++ lib/widgets/cards/my_ratio_card.dart
@@ -1,9 +1,7 @@
 import 'package:doffa/providers/metrics_provider.dart';
-import 'package:doffa/widgets/buttons/common/my_confirmation_dialog.dart';
 import 'package:doffa/widgets/my_container.dart';
 import 'package:doffa/widgets/text/my_montserrat.dart';
 import 'package:flutter/material.dart';
-import 'package:logger/logger.dart';
 import 'package:provider/provider.dart';
 
 import 'utils/motivational_scale.dart';
@@ -16,7 +14,6 @@ class MyRatioCard extends StatelessWidget {
     final metricsProvider = context.watch<MetricsProvider>();
     final ratio = metricsProvider.getRatio();
     final wordColor = getWordColor(ratio);
-    final logger = Logger(printer: SimplePrinter(colors: false));
 
     return LayoutBuilder(
       builder: (context, constraints) {
@@ -24,15 +21,7 @@ class MyRatioCard extends StatelessWidget {
 
         return GestureDetector(
           onTap: () {
-            showConfirmationDialog(
-              context: context,
-              title: 'You are awesome!',
-              message: 'Some description of ratio here',
-              confirmText: 'Go Back!',
-              onConfirm: () {
-                logger.i('Ratio description button pressed');
-              },
-            );
+            showInformationDialog(context: context, maxWidth: maxWidth);
           },
           child: MyContainer(
             maxWidth: maxWidth,
@@ -126,4 +115,186 @@ class BadgeContainer extends StatelessWidget {
       child: child,
     );
   }
+}
+
+void showInformationDialog({
+  required BuildContext context,
+  required double maxWidth,
+}) {
+  showDialog(
+    context: context,
+    builder:
+        (context) => AlertDialog(
+          title: MyMontserrat(
+            text: "What is the Doffa Ratio score??",
+            maxWidth: 14,
+          ),
+          content: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              spacing: 2,
+              children: [
+                _buildCalculationExplanation(context, maxWidth),
+                const SizedBox(height: 8),
+                _buildRatioCalulator(),
+                const SizedBox(height: 8),
+                ...motivationScale.entries.map((entry) {
+                  final ratio = entry.key;
+                  final word = entry.value.word;
+                  final color = entry.value.color;
+                  return _builbScaleGradeContainer(color, ratio, word);
+                }),
+              ],
+            ),
+          ),
+          backgroundColor: const Color(0xFF1E1E1E),
+          shape: RoundedRectangleBorder(
+            side: const BorderSide(color: Colors.white70),
+            borderRadius: BorderRadius.circular(8),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const MyMontserrat(
+                text: 'Ok',
+                maxWidth: 14,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ],
+        ),
+  );
+}
+
+Widget _buildRatioCalulator() {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    mainAxisSize: MainAxisSize.min,
+    children: [
+      MyMontserrat(
+        text: "Try it out yourself!",
+        maxWidth: 14,
+        fontWeight: FontWeight.w600,
+      ),
+      const SizedBox(height: 8),
+      MyMontserrat(
+        text: "Lean mass change (kg):",
+        maxWidth: 14,
+        fontWeight: FontWeight.w400,
+      ),
+    ],
+  );
+}
+
+Widget _buildCalculationExplanation(BuildContext context, double maxWidth) {
+  final TextStyle textStyle = MyMontserrat.defaultStyle().copyWith(
+    fontSize: 12,
+    color: Colors.white70,
+    height: 1.2,
+    fontWeight: FontWeight.w400,
+  );
+  final TextStyle textStyleBold = textStyle.copyWith(
+    fontSize: 14,
+    color: Colors.white,
+    fontWeight: FontWeight.w600,
+  );
+  final TextStyle textStyleRed = textStyle.copyWith(color: Colors.redAccent);
+  final TextStyle textStyleGreen = textStyle.copyWith(
+    color: Colors.greenAccent,
+  );
+
+  return SizedBox(
+    width: maxWidth - 100,
+    child: RichText(
+      text: TextSpan(
+        style: textStyle,
+        children: [
+          TextSpan(
+            text:
+                'The Doffa Ratio measures the quality of your body change\n— not just weight. It ranges from ',
+          ),
+          TextSpan(text: '-100 (bad)', style: textStyleRed),
+          TextSpan(text: ' to '),
+          TextSpan(text: '+100 (excellent)', style: textStyleGreen),
+          TextSpan(text: '.\n\n'),
+
+          TextSpan(text: 'Formula:\n', style: textStyleBold),
+          TextSpan(
+            text: 'Score = ((ΔLean - ΔFat) / |ΔLean + ΔFat|) x 100\n\n',
+            style: textStyle,
+          ),
+
+          TextSpan(text: 'Where:\n', style: textStyleBold),
+          TextSpan(text: '• ΔLean = change in lean mass\n'),
+          TextSpan(text: '• ΔFat = change in fat mass\n\n'),
+
+          TextSpan(text: 'Examples:\n', style: textStyleBold),
+          TextSpan(text: '• Lost 5kg fat → Score: '),
+          TextSpan(text: '+100', style: textStyle),
+          TextSpan(text: ' (Pure fat loss)\n'),
+
+          TextSpan(text: '• Gained 3kg muscle → Score: '),
+          TextSpan(text: '+100', style: textStyle),
+          TextSpan(text: ' (Pure lean gain)\n'),
+
+          TextSpan(text: '• Gained 3kg fat, 1kg lean → Score: '),
+          TextSpan(text: '-50', style: textStyle),
+          TextSpan(text: ' (Mostly fat gain)\n'),
+
+          TextSpan(text: '• Lost 2kg fat, gained 4kg lean → Score: '),
+          TextSpan(text: '+75', style: textStyle),
+          TextSpan(text: ' (Great recomposition on bulk)'),
+        ],
+      ),
+    ),
+  );
+}
+
+Widget _builbScaleGradeContainer(Color color, int ratio, String word) {
+  final hsl = HSLColor.fromColor(color);
+  final Color secondaryColor =
+      hsl.withLightness((hsl.lightness * 0.7).clamp(0.0, 1.0)).toColor();
+
+  return Container(
+    width: double.infinity,
+    decoration: BoxDecoration(
+      gradient: LinearGradient(
+        colors: [color, secondaryColor, Colors.transparent, Colors.transparent],
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+      ),
+    ),
+    child: Padding(
+      padding: const EdgeInsets.all(2.0),
+      child: Row(
+        spacing: 8,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            flex: 7,
+            child: Align(
+              alignment: Alignment.centerRight,
+              child: MyMontserrat(
+                text: ratio.toString(),
+                maxWidth: 10,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: Align(
+              alignment: Alignment.centerRight,
+              child: MyMontserrat(
+                text: word,
+                maxWidth: 10,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
 }
