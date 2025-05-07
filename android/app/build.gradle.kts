6da9066c25a5b7f0dc1331180d702e33e80ad0b8
Joel Setterberg <j.settrbrg@gmail.com>
2025-05-07 21:38:48 +0200

    Changed namespace on android

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.doffa"
+    namespace = "com.nangidev.doffa"
     compileSdk = flutter.compileSdkVersion
     // ndkVersion = flutter.ndkVersion
     ndkVersion = "28.1.13356709"
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.doffa"
+        applicationId = "com.nangidev.doffa"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
