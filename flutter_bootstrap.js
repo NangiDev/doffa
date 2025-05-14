5cf27eceb5a70d34d2ff698caeb3d89a91ad697a
NangiDev <NangiDev@users.noreply.github.com>
2025-05-14 05:46:21 +0000

    deploy: 4f3df7aa6718bc7a96a8f0776ae1cb9f32546316

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "812155579"
+    serviceWorkerVersion: "1919194106"
   }
 });
