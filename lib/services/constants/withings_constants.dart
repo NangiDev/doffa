7e483fcd000f8ed6e9e34ad1e59e3288d8149013
Joel Setterberg <j.settrbrg@gmail.com>
2025-05-21 14:47:42 +0200

    Full Withings auth flow is now through firebase

--- lib/services/constants/withings_constants.dart
+++ lib/services/constants/withings_constants.dart
@@ -1,26 +1,6 @@
+import 'package:flutter/foundation.dart' show kIsWeb;
+
 class WithingsConstants {
-  static const String _webProdUrl =
-      "https://nangidev.github.io/doffa/auth.html";
-  static const String _webDevUrl = "http://localhost:3001/doffa/auth.html";
-
-  static const String responseType = "code";
-  static const String clientId =
-      "cb2184c8dd2920bc6707956eb8da529f319316f154b659d56e04e1714bed2dcf";
-  static const String scope = "user.metrics";
-  static const String state = "FLUTTER_AUTH";
   static final String env = Uri.base.host == "localhost" ? "dev" : "prod";
-
-  static String get redirectUri {
-    return Uri.base.host == "localhost" ? _webDevUrl : _webProdUrl;
-  }
-
-  static const String callbackUrlScheme = "http";
-
-  static const String baseUrl =
-      "https://account.withings.com/oauth2_user/authorize2";
-
-  // Fetches the 30 sec code for using when fetching the access token
-  static String getOAuthUrl() {
-    return "$baseUrl?response_type=$responseType&client_id=$clientId&redirect_uri=$redirectUri&scope=$scope&state=$state";
-  }
+  static final String target = kIsWeb ? "web" : "mobile";
 }
