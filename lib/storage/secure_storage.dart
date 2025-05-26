734c47808f09b323912e31552c507cb202196e0b
Joel Setterberg <j.settrbrg@gmail.com>
2025-05-26 13:18:39 +0200

    Some kind of cache

--- lib/storage/secure_storage.dart
+++ lib/storage/secure_storage.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:doffa/common/models.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'storage.dart';
 
@@ -16,6 +19,24 @@ class SecureStorage implements Storage {
   }
 
   @override
+  Future<void> writeCache(StorageKeys key, Map<String, Metrics> value) async {
+    // Ensure the data is encoded into JSON
+    final encoded = jsonEncode(
+      value.map(
+        (k, v) => MapEntry(k, v.toJson()),
+      ), // Convert each Metrics object to JSON
+    );
+
+    await _storage.write(
+      key: "${StorageKeys.cache.name}Count",
+      value: value.length.toString(),
+    );
+
+    // Store the encoded data in secure storage
+    await _storage.write(key: key.name, value: encoded);
+  }
+
+  @override
   Future<String> read(StorageKeys key) async {
     return await _storage.read(key: key.name) ?? "";
   }
@@ -24,6 +45,28 @@ class SecureStorage implements Storage {
   Future<bool> readBool(StorageKeys key) async {
     String valueStr = await _storage.read(key: key.name) ?? "";
     return valueStr == "true";
+  }
+
+  @override
+  Future<Map<String, Metrics>> readCache(StorageKeys key) async {
+    // Read the raw string from storage
+    final str = await _storage.read(key: key.name);
+    if (str == null || str.isEmpty) {
+      return {}; // Return an empty map if no data found
+    }
+
+    try {
+      // Decode the JSON string into a Map<String, dynamic>
+      final decoded = jsonDecode(str) as Map<String, dynamic>;
+
+      // Convert each value in the map from a Map<String, dynamic> to a Metrics object
+      return decoded.map(
+        (k, v) => MapEntry(k, Metrics.fromJson(v as Map<String, dynamic>)),
+      );
+    } catch (e) {
+      // If there is an error during decoding, log it or handle the failure
+      return {}; // Return an empty map in case of an error
+    }
   }
 
   @override
--- lib/storage/storage.dart
+++ lib/storage/storage.dart
@@ -1,3 +1,5 @@
+import 'package:doffa/common/models.dart';
+
 enum StorageKeys {
   accessToken,
   start,
@@ -6,13 +8,16 @@ enum StorageKeys {
   expandedHistory,
   expandedProgress,
   platformProvider,
+  cache,
 }
 
 abstract class Storage {
   Future<void> write(StorageKeys key, String value);
   Future<void> writeBool(StorageKeys key, bool value);
+  Future<void> writeCache(StorageKeys key, Map<String, Metrics> value);
   Future<String> read(StorageKeys key);
   Future<bool> readBool(StorageKeys key);
+  Future<Map<String, Metrics>> readCache(StorageKeys key);
   Future<void> delete(StorageKeys key);
   Future<void> clear();
 }
