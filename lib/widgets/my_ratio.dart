3106642a200c6b25ca3a87d53e00e68fe0f35a9e
Joel Setterberg <j.settrbrg@gmail.com>
2025-05-26 13:18:38 +0200

    Done with flutter version on par with old version

--- lib/widgets/my_ratio.dart
+++ lib/widgets/my_ratio.dart
@@ -1,24 +1,30 @@
+import 'package:doffa/api/api_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class MyRatio extends StatelessWidget {
   const MyRatio({super.key});
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Flex(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          direction: Axis.vertical,
-          children: [
-            Text(
-              '80/20',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
+    return Consumer<ApiProvider>(
+      builder: (context, apiProvider, child) {
+        return Card(
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Flex(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              direction: Axis.vertical,
+              children: [
+                Text(
+                  "${apiProvider.ratio.lean}/${apiProvider.ratio.fat}",
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
